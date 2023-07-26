598a550a58a339d068085a377113ff1ee368e049
TK-ed <tharunstark11@gmail.com>
2023-07-26 12:10:34 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# imports
+from fastapi import FastAPI
+from pydantic import BaseModel
+# from pymongo.mongo.client import MongoClient
+from dotenv import *
+import os
+import uvicorn
+
+app = FastAPI()
+
+# declarations
+db = os.environ.get("MONG_URI")
+
+@app.get("/")
+async def root():
+    db = os.environ.get("BRUHH")
+    print(db) 
+       
+@app.get("/todos")
+def get_todos():
+    return {"todos": {"id1": "todo1", "id2": "todo2"}}
+
+class Todo(BaseModel):
+    todo: str
+    id: int
+    
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port = 6969, reload = True)
